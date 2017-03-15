1902541da1466988a48a7f16e30f5fac9b6263c9
choueric <zhssmail@gmail.com>
2017-03-15 15:48:53 +0800

    add -c for 'list' command

--- cmd_list.go
+++ cmd_list.go
@@ -23,23 +23,30 @@ import (
 	"io"
 )
 
-var listVerbose bool
-
 func listUsage(w io.Writer, m *helpMap) {
-	cmdTitle("list [-v]", false)
+	cmdTitle("list [-v|-c]", false)
 	cmdInfo("List all profiles.\n")
-	cmdInfo("-v: Print with more information\n\n")
+	cmdInfo("-v: Print with more information\n")
+	cmdInfo("-c: Print full information of current profile.\n\n")
 }
 
 func listHandler(args []string, data interface{}) (int, error) {
 	fmt.Printf("cmd '%s':\n", cWrap(cGREEN, "list"))
 
+	var verbose, current bool
 	flagSet := flag.NewFlagSet("list", flag.ExitOnError)
-	flagSet.BoolVar(&listVerbose, "v", false, "print with more information")
+	flagSet.BoolVar(&verbose, "v", false, "print with more information")
+	flagSet.BoolVar(&current, "c", false, "print all information of current profile")
 	flagSet.Parse(args)
 
+	if current {
+		p := gConfig.Profiles[gConfig.Current]
+		printProfile(&p, true, true, gConfig.Current)
+		return 0, nil
+	}
+
 	for i, p := range gConfig.Profiles {
-		printProfile(&p, listVerbose, gConfig.Current == i, i)
+		printProfile(&p, verbose, gConfig.Current == i, i)
 	}
 
 	return 0, nil
