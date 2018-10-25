281fa5371ce4b6873f0cff67e30b100e31f56f43
Haishan Zhou <Haishan.Zhou@invenco.com>
2018-10-25 14:58:14 +1300

    do not copy dtb anymore
    The copy operation creates a dtb file in the root of build directory.
    This file has the same name of the target of Makefile to build dtb, so
    it lets make think the target is new and do nothing to build dtb.

--- cmd_build.go
+++ cmd_build.go
@@ -57,11 +57,5 @@ func buildDtbHandler(args []string, data interface{}) (int, error) {
 		return 0, err
 	}
 
-	src := p.BuildDir + "/arch/" + p.Arch + "/boot/dts/" + p.DTB
-	dst := p.BuildDir + "/" + p.DTB
-
-	if err := copyFileContents(src, dst); err != nil {
-		return 0, err
-	}
 	return 0, nil
 }
